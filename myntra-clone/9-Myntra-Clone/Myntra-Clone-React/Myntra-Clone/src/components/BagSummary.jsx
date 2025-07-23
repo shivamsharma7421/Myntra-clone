ba69e9a8e8f061cc0f8e5301a8d7334fe2aa9ba8
Your Name <shivamsharma7421@gmail.com>
2025-07-23 14:18:46 +0530

    UI part done

---
+++ myntra-clone/9-Myntra-Clone/Myntra-Clone-React/Myntra-Clone/src/components/BagSummary.jsx
@@ -0,0 +1,39 @@
+const BagSummary = ({ }) => {
+
+  const bagsummary = {
+    totalItem: 3,
+    totalMRP: 5000,
+    totalDiscount: 1000,
+    finalPayment: 4100
+  }
+  return (
+    <div className="bag-summary">
+
+      <div className="bag-details-container">
+        <div className="price-header">PRICE DETAILS ({bagsummary.totalItem} Items) </div>
+        <div className="price-item">
+          <span className="price-item-tag">Total MRP</span>
+          <span className="price-item-value">₹{bagsummary.totalMRP}</span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Discount on MRP</span>
+          <span className="price-item-value priceDetail-base-discount">-₹{bagsummary.totalDiscount}</span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Convenience Fee</span>
+          <span className="price-item-value">₹99</span>
+        </div>
+        <hr />
+        <div className="price-footer">
+          <span className="price-item-tag">Total Amount</span>
+          <span className="price-item-value">₹{bagsummary.finalPayment}</span>
+        </div>
+      </div>
+      <button className="btn-place-order">
+        <div className="css-xjhrni">PLACE ORDER</div>
+      </button>
+    </div>
+
+  );
+}
+export default BagSummary;
